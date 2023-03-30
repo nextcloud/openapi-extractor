f333298a8457a5e75056d18aad006d98a1543851
jld3103 <jld3103yt@gmail.com>
2023-03-30 08:57:09 +0200

    Add http bearer authentication

--- src/common.php
+++ src/common.php
@@ -2,8 +2,34 @@
 
 namespace OpenAPIExtractor;
 
+use cebe\openapi\spec\SecurityRequirement;
+
 function generateReadableAppID(string $appID): string {
 	return implode("", array_map(fn(string $s) => ucfirst($s), explode("_", $appID)));
+}
+
+function securitySchemes(): array {
+	return [
+		"basic_auth" => [
+			"type" => "http",
+			"scheme" => "basic",
+		],
+		"bearer_auth" => [
+			"type" => "http",
+			"scheme" => "bearer",
+		],
+	];
+}
+
+function securityRequirements(): array {
+	return [
+		[
+			"basic_auth" => [],
+		],
+		[
+			"bearer_auth" => [],
+		],
+	];
 }
 
 function jsonFlags(): int {
