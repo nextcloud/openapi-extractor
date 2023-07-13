2de0dc30339b33ba359379c94b8212cd2cb8cdad
jld3103 <jld3103yt@gmail.com>
2023-07-13 10:32:23 +0200

    Colorize the log output

--- src/logger.php
+++ src/logger.php
@@ -4,34 +4,45 @@ namespace OpenAPIExtractor;
 
 use Exception;
 
+enum LoggerColor {
+	case Green;
+	case Yellow;
+	case Red;
+}
+
 class Logger {
 	static bool $exitOnError = true;
 
-	protected static function log(string $level, string $context, string $text): void {
-		print(self::format($level, $context, $text));
+	protected static function log(LoggerColor $color, string $level, string $context, string $text): void {
+		print(self::format($color, $level, $context, $text));
 	}
 
-	protected static function format(string $level, string $context, string $text): string {
-		return $level . ": " . $context . ": " . $text . "\n";
+	protected static function format(LoggerColor $color, string $level, string $context, string $text): string {
+		$colorCode = match ($color) {
+			LoggerColor::Green => "",
+			LoggerColor::Yellow => "\e[33m",
+			LoggerColor::Red => "\e[91m",
+		};
+		return $colorCode . $level . ": " . $context . ": " . $text . "\n\e[0m";
 	}
 
 	public static function info(string $context, string $text): void {
-		self::log("Info", $context, $text);
+		self::log(LoggerColor::Green, "Info", $context, $text);
 	}
 
 	public static function warning(string $context, string $text): void {
-		self::log("Warning", $context, $text);
+		self::log(LoggerColor::Yellow, "Warning", $context, $text);
 	}
 
 	public static function error(string $context, string $text): void {
 		if (self::$exitOnError) {
-			throw new Exception(self::format("Error", $context, $text));
+			throw new Exception(self::format(LoggerColor::Red, "Error", $context, $text));
 		} else {
-			self::log("Error", $context, $text);
+			self::log(LoggerColor::Red, "Error", $context, $text);
 		}
 	}
 
 	public static function panic(string $context, string $text): void {
-		throw new Exception(self::format("Panic", $context, $text));
+		throw new Exception(self::format(LoggerColor::Red, "Panic", $context, $text));
 	}
 }
