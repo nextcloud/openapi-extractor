fb82c9e35416921613a6ae633d9ef53d35f595b7
provokateurin <kate@provokateurin.de>
2024-01-10 16:27:55 +0100

    refactor(routing): Allow unknown routes.php formats
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- src/Route.php
+++ src/Route.php
@@ -22,18 +22,19 @@ class Route {
 
 	public static function parseRoutes(string $path): array {
 		$content = file_get_contents($path);
-		if (str_contains($content, "return ")) {
-			if (str_contains($content, "\$this")) {
-				preg_match("/return ([^;]*);/", $content, $matches);
-				return self::includeRoutes("<?php\nreturn " . $matches[1] . ";");
+		if (str_contains($content, 'return ')) {
+			if (str_contains($content, '$this')) {
+				preg_match('/return ([^;]*);/', $content, $matches);
+				return self::includeRoutes("<?php\nreturn ${matches[1]};");
 			}
 			return include($path);
-		} elseif (str_contains($content, "registerRoutes")) {
-			preg_match_all("/registerRoutes\(.*?\\\$this,.*?(\[[^;]*)\);/s", $content, $matches);
-			return array_merge(...array_map(fn (string $match) => self::includeRoutes("<?php\nreturn " . $match . ";"), $matches[1]));
-		} else {
-			Logger::panic("Routes", "Unknown routes.php format");
+		} elseif (str_contains($content, 'registerRoutes')) {
+			preg_match_all('/registerRoutes\(.*?\$this,.*?(\[[^;]*)\);/s', $content, $matches);
+			return array_merge(...array_map(fn (string $match) => self::includeRoutes("<?php\nreturn $match;"), $matches[1]));
 		}
+
+		Logger::warning('Routes', 'Unknown routes.php format');
+		return [];
 	}
 
 	private static function includeRoutes(string $code): array {
